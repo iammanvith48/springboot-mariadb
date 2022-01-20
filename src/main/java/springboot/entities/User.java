783f2660129dd785025f170b940f95f431e1f9a9
iammanvith48 <97126491+iammanvith48@users.noreply.github.com>
2022-01-20 17:12:44 +0530

    Add files via upload

---
+++ src/main/java/springboot/entities/User.java
@@ -0,0 +1,43 @@
+package springboot.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+	@Column(name="firstname")
+    private String firstname;
+	@Column(name="lastname")
+    private String lastname;
+    public User() {
+    }
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+    
+
+
+}
